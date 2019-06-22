ec9a5172f10626f709c3c1a6166cf47b83df43e9
SuryaMahindra <reddy.nallabathuni@mahindra.com>
2019-06-22 07:54:44 +0530

    second commit

--- continuouscamera/src/main/java/com/mfc/continuouscamera/Example.java
+++ continuouscamera/src/main/java/com/mfc/continuouscamera/Example.java
@@ -1,4 +1,11 @@
 package com.mfc.continuouscamera;
 
+import android.util.Log;
+
 public class Example {
+
+    public void addition(int a , int b){
+        int res = a+b;
+        Log.e("the adition ","the addition "+res);
+    }
 }
